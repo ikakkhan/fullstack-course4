47184cfb81303678dea99116c1d288119a312d69
ikakkhan <2250@psuwitsurat.ac.th>
2024-05-07 23:47:06 +0700

    lect41

--- examples/Lecture41/js/script.js
+++ examples/Lecture41/js/script.js
@@ -1,15 +1,15 @@
-var message = "in global";
-console.log("global: message = " + message);
+var message = "inside global";
+console.log("global message = " + message);
 
 var a = function () {
-  var message = "inside a";
-  console.log("a: message = " + message);
+    var message = "inside a";
+    console.log("a message = " + message);
 
-  function b () {
-    console.log("b: message = " + message); 
-  }
-
-  b();
+    function b () {
+        console.log("b message = " + message);
+    }
+    
+    b();
 }
 
 a();
