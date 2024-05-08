994faa50da4584144b8b7c06bd03b0d5f0d858a6
ikakkhan <2250@psuwitsurat.ac.th>
2024-05-08 17:12:34 +0700

    lect42

--- examples/Lecture42/js/script.js
+++ examples/Lecture42/js/script.js
@@ -1,16 +1,13 @@
-// should be undefined
 var x;
 console.log(x);
 
-if (x == undefined) {
-  console.log("x is undefined");
-}
+// if (x == undefined) {
+//   console.log("x is undefined");
+// }
 
-x = 5;
-if (x == undefined) {
-  console.log("x is undefined");
-}
-else {
-  console.log("x has been defined");
-}
-
+// x =  5;
+// if (x == undefined) {
+//   console.log("x is undefined");
+// } else {
+//   console.log("x has been defined");
+// }
