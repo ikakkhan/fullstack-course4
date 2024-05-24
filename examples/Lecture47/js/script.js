a53ef56c13d61c374f508bc085a51580fed27bcd
khan <2250@psuwitsurat.ac.th>
2024-05-24 17:07:30 +0700

    lect47

--- examples/Lecture47/js/script.js
+++ examples/Lecture47/js/script.js
@@ -1,65 +1,68 @@
 // Copy by Reference vs by Value
+//copy by value(primitive)
 var a = 7;
 var b = a;
+
 console.log("a: " + a);
 console.log("b: " + b);
 
 b = 5;
-console.log("after b update:");
+
+console.log("after b update");
 console.log("a: " + a);
 console.log("b: " + b);
 
-
-
-var a = { x: 7 };
-var b = a;
+//copy by reference(object)
+a =  {
+  x : 7,
+  y : 40
+};
+b = a;
 console.log(a);
 console.log(b);
+
+console.log("a: " + a.x);
+console.log("b: " + b.x);
 
 b.x = 5;
-console.log("after b.x update:");
+
 console.log(a);
 console.log(b);
 
+console.log("after b update");
+console.log("a: " + a.x);
+console.log("b: " + b.x);
 
+// pass by Reference vs by Value
+//pass by value(primitive)
+function changeprim(prim) {
+  console.log("in change primitive")
+  console.log("before: " + prim);
 
-
-
-
-// Pass by reference vs by value
-function changePrimitive(primValue) {
-  console.log("in changePrimitive...");
-  console.log("before:");
-  console.log(primValue);
-  
-  primValue = 5;
-  console.log("after:");
-  console.log(primValue);
+  prim = 5;
+  console.log("after: " + prim);
 }
 
 var value = 7;
-changePrimitive(value); // primValue = value
-console.log("after changePrimitive, orig value:");
-console.log(value);
+changeprim(value);
+console.log("after changeprim, value: " + value);
 
+// pass by Reference(object)
+function changeobj(obj) {
+  console.log("in change of obeject");
+  console.log(obj);
+  console.log("before: " + obj.x);
 
-
-function changeObject(objValue) {
-  console.log("in changeObject...");
-  console.log("before:");
-  console.log(objValue);
-  
-  objValue.x = 5;
-  console.log("after:");
-  console.log(objValue);
+  obj.x = 5;
+  console.log(obj);
+  console.log("after: " + obj.x);
 }
 
-value = { x: 7 };
-changeObject(value); // objValue = value
-console.log("after changeObject, orig value:");
+value = {
+  x : 7,
+  y : 30
+};
+changeobj(value);
+console.log("after changeobj");
 console.log(value);
-
-
-
-
-
+console.log("value.x: " + value.x);
